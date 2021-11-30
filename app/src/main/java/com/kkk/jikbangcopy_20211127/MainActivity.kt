32d91d237e867f4c447b7438fe48245b1871c888
Kunote <rndudah754@gmail.com>
2021-11-30 17:00:38 +0900

    방 목록 클릭시 -> 상세화면으로 단순 이동

--- app/src/main/java/com/kkk/jikbangcopy_20211127/MainActivity.kt
+++ app/src/main/java/com/kkk/jikbangcopy_20211127/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kkk.jikbangcopy_20211127
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kkk.jikbangcopy_20211127.adapters.RoomAdapter
@@ -33,6 +34,16 @@ class MainActivity : AppCompatActivity() {
 
         mRoomAdapter = RoomAdapter(this,R.layout.room_list_item, mRoomList)
         roomListView.adapter = mRoomAdapter
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedRoom = mRoomList[position]
+
+            val myIntent = Intent( this,ViewRoomDetailActivity::class.java )
+            startActivity(myIntent)
+
+
+        }
 
 
     }
