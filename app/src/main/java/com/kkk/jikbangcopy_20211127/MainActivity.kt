9b96b6943dd62cccff89709b6c89ebebdb356e16
Kunote <rndudah754@gmail.com>
2021-11-30 13:01:42 +0900

    방 목록 변수 생성   - 실제 방 데이터들을 추가 (직접 타이핑)

--- app/src/main/java/com/kkk/jikbangcopy_20211127/MainActivity.kt
+++ app/src/main/java/com/kkk/jikbangcopy_20211127/MainActivity.kt
@@ -2,10 +2,32 @@ package com.kkk.jikbangcopy_20211127
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kkk.jikbangcopy_20211127.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mRoomList.add( RoomData(8000,"서울시 동대문구", 10, "1번째 방입니다") )
+        mRoomList.add( RoomData(18000,"서울시 동대문구", 9, "2번째 방입니다") )
+        mRoomList.add( RoomData(25000,"서울시 동대문구", 8, "3번째 방입니다") )
+        mRoomList.add( RoomData(98000,"서울시 서대문구", 1, "4번째 방입니다") )
+        mRoomList.add( RoomData(8700,"서울시 서대문구", 2, "5번째 방입니다") )
+        mRoomList.add( RoomData(6000,"서울시 종로구", 5, "6번째 방입니다") )
+        mRoomList.add( RoomData(2300,"서울시 용산구", 4, "7번째 방입니다") )
+        mRoomList.add( RoomData(1100,"서울시 종로구", 5, "8번째 방입니다") )
+        mRoomList.add( RoomData(26700,"서울시 종로구", -1, "9번째 방입니다") )
+        mRoomList.add( RoomData(34500,"서울시 종로구", -2, "10번째 방입니다") )
+        mRoomList.add( RoomData(9800,"서울시 용산구", 2, "11번째 방입니다") )
+        mRoomList.add( RoomData(1110,"서울시 서대문구", 3, "12번째 방입니다") )
+        mRoomList.add( RoomData(4720,"서울시 동대문구", 0, "13번째 방입니다") )
+        mRoomList.add( RoomData(2310,"서울시 서대문구", -1, "14번째 방입니다") )
+
+
     }
 }
